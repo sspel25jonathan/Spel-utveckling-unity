b32f091cf649df2aab6fbc661a2ad483be0716bf
jonathan <sspel25jope@axevalla.fhsk.se>
2025-09-12 11:51:07 +0200

    colldown

--- Assets/Assets for space/Axetutorial-main/Assets/Scripts/FoeBehavior.cs
+++ Assets/Assets for space/Axetutorial-main/Assets/Scripts/FoeBehavior.cs
@@ -1,3 +1,4 @@
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class FoeBehavior : MonoBehaviour
@@ -5,7 +6,7 @@ public class FoeBehavior : MonoBehaviour
 
     public GameObject enemy;
     public float enemyhp = 1f;
-
+    public float horizontalMovmet = 1f;
     public float eneymyShootCooldown = 1f;
     public float enemySpeed = 2f;
 
@@ -15,26 +16,47 @@ public class FoeBehavior : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * enemySpeed * Time.deltaTime);
 
-        if (enemyhp == 0)
+        float randomLeftRight = Random.Range(0, 1);
+        if (randomLeftRight == 1)
         {
-            Destroy(gameObject);
+            transform.Translate(Vector3.left * horizontalMovmet  * Time.deltaTime * 3);
         }
+        else if (randomLeftRight == 0)
+        {
+             transform.Translate(Vector3.right * horizontalMovmet * Time.deltaTime * 3);
+        }
+
+        transform.Translate(Vector3.down * enemySpeed * Time.deltaTime);
 
         if (transform.position.y < -11)
         {
             float randnumspawn = Random.Range(-5, 5);
             transform.position = new Vector2(randnumspawn, 11);
         }
+    }
 
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player Bullet")
+        {
+            enemyhp = enemyhp - 1;
+            if (enemyhp == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
 
     }
 }
--- Assets/Assets for space/Axetutorial-main/Assets/Scripts/playerScript.cs
+++ Assets/Assets for space/Axetutorial-main/Assets/Scripts/playerScript.cs
@@ -6,23 +6,28 @@ public class first : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-  
+
     public float playerspeeed = 5f;
 
     public GameObject Bullet;
     public float Playerhealth = 3f;
+    float minTimer;
+
+    public float cooldown = 2f;
     void Start()
     {
         this.transform.position = new Vector3(0, -1, 0);
         /* wwaaa
          * 
          */
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * playerspeeed * Time.deltaTime);
@@ -39,12 +44,15 @@ public class first : MonoBehaviour
         {
             transform.Translate(Vector3.down * playerspeeed * Time.deltaTime);
         }
-
-        if (Input.GetKey(KeyCode.Space))
+        
+        
+        if (Input.GetKey(KeyCode.Space) && minTimer <= Time.time)
         {
-
             Instantiate(Bullet, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.rotation);
+            minTimer = Time.time + cooldown;
         }
+        Debug.Log("Time is:" + Time.time + " and CD is: " + minTimer);
+
 
         if (transform.position.x <= -11)
         {
@@ -64,6 +72,17 @@ public class first : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, -10.9f, 0);
         }
-        
+
     }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Playerhealth = Playerhealth - 1;
+            if (Playerhealth == 0)
+            {
+                Destroy(gameObject);
+            }
+            }
+         }
+
 }
